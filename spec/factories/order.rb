ae09171f0343b4a8d7e5b375288162de98efcf0d
Semen Pupkov <semen.pupkov@gmail.com>
2014-10-05 02:01:20 +0600

    Add factory for order_items

--- spec/factories/order.rb
+++ spec/factories/order.rb
@@ -1,4 +1,13 @@
 FactoryGirl.define do
   factory :order do
   end
+
+  factory :order_item do
+    association :order
+    association :product
+
+    price 500
+    product_title { product.name }
+    quantity 1
+  end
 end
