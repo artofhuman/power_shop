2279ec8bfc747f0de7904785f4cac0ba7bcee6bd
Semyon Pupkov <semen.pupkov@gmail.com>
2016-01-05 08:53:42 +0000

    Fix styles

--- app/models/power_shop/category.rb
+++ app/models/power_shop/category.rb
@@ -1,15 +1,12 @@
 module PowerShop
   class Category < ActiveRecord::Base
     self.abstract_class = true
-    self.table_name = 'categories'
+    self.table_name = 'categories'.freeze
 
     acts_as_nested_set
 
-    has_many :products, :dependent => :destroy,
-      class_name: '::Product'
-
-    has_one :image, :class_name => '::ShopImage', :as => :subject,
-      :dependent => :destroy
+    has_many :products, dependent: :destroy, class_name: '::Product'
+    has_one :image, class_name: '::ShopImage', as: :subject, dependent: :destroy
 
     accepts_nested_attributes_for :image, :allow_destroy => true
 
